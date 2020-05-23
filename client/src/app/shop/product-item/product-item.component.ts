4893e030c750b60d92ab3d60adad3b550fa18e63
rogong <itingasng@gmail.com>
2020-05-23 19:53:29 +0100

    Section 10 Client Routing

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent implements OnInit {
-
   @Input() product: IProduct;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,12 +5,14 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
+  product: IProduct;
 
   constructor(private http: HttpClient) {}
 
@@ -40,6 +42,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
